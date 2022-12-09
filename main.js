80b7efa0c459bca884bbaa94ca6e8f9f6f6f99e8
hankfontaine <hankfontaine@gmail.com>
2022-12-09 14:40:17 -0500

    worked on css

---
+++ main.js
@@ -0,0 +1,13 @@
+const body = document.querySelector("body")
+
+const header = document.createElement("header")
+header.append("header")
+body.appendChild(header)
+
+const main = document.createElement("main")
+main.append("main")
+body.appendChild(main)
+
+const footer = document.createElement("footer")
+footer.append("footer")
+body.appendChild(footer)
