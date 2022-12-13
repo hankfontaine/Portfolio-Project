01016cce1ef9df1f8eb09d471f68ba7c076ab6da
hankfontaine <hankfontaine@gmail.com>
2022-12-13 15:00:11 -0500

    added formatting

--- main.js
+++ main.js
@@ -9,7 +9,12 @@ main.append("main")
 body.appendChild(main)
 
 const footer = document.createElement("footer")
-footer.append("footer")
+footer.append("CONTACT ME")
+footer.append("name")
+footer.append("address")
+footer.append("phone")
+footer.append("email")
+footer.append("links")
 body.appendChild(footer)
 
 const projectList = document.createElement("ul")
@@ -20,5 +25,6 @@ for (let i = 0; i <= 5; i++) {
     let div = document.createElement("li")
     div.classList.add("project-container")
     div.append("test")
+
     projectList.appendChild(div)
 }
