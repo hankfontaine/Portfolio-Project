1464ae2f06e2adf763016dba9c2a5a7f0debb845
hankfontaine <hankfontaine@gmail.com>
2022-12-14 14:52:28 -0500

    set up file

--- main.js
+++ main.js
@@ -1,48 +1,52 @@
-const body = document.querySelector("body")
+const body = document.querySelector('body');
 
 /////////////////////////////////////////////////////////////
 //////////////////// HEADER COMPONENTS //////////////////////
 /////////////////////////////////////////////////////////////
 
-const header = document.createElement("header")
-header.append("header")
-body.appendChild(header)
+const header = document.createElement('header');
+header.append('header');
+body.appendChild(header);
+
+const headerContainer = document.createElement('div');
+headerContainer.classList.add('header-container');
+header.appendChild(headerContainer);
 
 /////////////////////////////////////////////////////////////
 //////////////////// MAIN COMPONENTS ////////////////////////
 /////////////////////////////////////////////////////////////
 
-const main = document.createElement("main")
-body.appendChild(main)
+const main = document.createElement('main');
+body.appendChild(main);
 
-const bioContainer = document.createElement("div")
-bioContainer.append("About me.  bio goes here")
-main.appendChild(bioContainer)
+const bioContainer = document.createElement('div');
+bioContainer.append('About me.  bio goes here');
+main.appendChild(bioContainer);
 
-const bioLinksContainer = document.createElement("div")
-bioLinksContainer.append("LINKS ")
-main.appendChild(bioLinksContainer)
+const bioLinksContainer = document.createElement('div');
+bioLinksContainer.append('LINKS ');
+main.appendChild(bioLinksContainer);
 
-const projectList = document.createElement("ul")
-projectList.classList.add("project-list")
-main.appendChild(projectList)
+const projectList = document.createElement('ul');
+projectList.classList.add('project-list');
+main.appendChild(projectList);
 
-for (let i = 0; i <= 5; i++) {
-    let div = document.createElement("li")
-    div.classList.add("project-container")
-    div.append("test")
+for (let i = 0; i < 1; i++) {
+	let div = document.createElement('li');
+	div.classList.add('project-container');
+	div.append('test');
 
-    projectList.appendChild(div)
+	projectList.appendChild(div);
 }
 /////////////////////////////////////////////////////////////
 ///////////////////// FOOTER COMPONENTS /////////////////////
 /////////////////////////////////////////////////////////////
 
-const footer = document.createElement("footer")
-footer.append("CONTACT ME")
-footer.append("name")
-footer.append("address")
-footer.append("phone")
-footer.append("email")
-footer.append("links")
-body.appendChild(footer)
+const footer = document.createElement('footer');
+footer.append('CONTACT ME');
+footer.append('name');
+footer.append('address');
+footer.append('phone');
+footer.append('email');
+footer.append('links');
+body.appendChild(footer);
